40cccfb68e79e7a4fd868c1d3086d6876a13557a
xixotron <xixotron89@gmail.com>
2025-09-18 19:51:53 +0200

    normalize url and tests

---
+++ normalize_url.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/url"
+	"path"
+	"strings"
+)
+
+func normalizeURL(rawURL string) (string, error) {
+	url, err := url.Parse(rawURL)
+	if err != nil {
+		return "", err
+	}
+
+	url.Scheme = ""
+	url.Path = url.EscapedPath()
+	url.Path = strings.ReplaceAll(url.Path, "//", "/")
+	url.Path = path.Clean(url.Path)
+	if url.Path[len(url.Path)-1] == '/' {
+		url.Path = url.Path[:len(url.Path)-1]
+	}
+	url.Host = strings.ToLower(url.Host)
+
+	return url.Host + url.Path, nil
+}
---
+++ normalize_url_test.go
@@ -0,0 +1,48 @@
+package main
+
+import "testing"
+
+func Test_normalizeURL(t *testing.T) {
+	tests := map[string]struct {
+		inputURL string
+		expected string
+	}{
+		"remove https scheme": {
+			inputURL: "https://blog.boot.dev/path",
+			expected: "blog.boot.dev/path",
+		},
+		"remove http scheme": {
+			inputURL: "https://blog.boot.dev/path/",
+			expected: "blog.boot.dev/path",
+		},
+		"remove final separator": {
+			inputURL: "https://blog.boot.dev/path/",
+			expected: "blog.boot.dev/path",
+		},
+		"remove duplicate slashes": {
+			inputURL: "http://blog.boot.dev//path//",
+			expected: "blog.boot.dev/path",
+		},
+		"remove dot segments": {
+			inputURL: "http://blog.boot.dev/./path/../",
+			expected: "blog.boot.dev",
+		},
+		"lowercase host part": {
+			inputURL: "http://BLOG.BOOT.DEV/path",
+			expected: "blog.boot.dev/path",
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			actual, err := normalizeURL(tc.inputURL)
+			if err != nil {
+				t.Errorf("Test '%s' FAIL: unexpected error: %v", name, err)
+				return
+			}
+			if actual != tc.expected {
+				t.Errorf("Test '%s' FAIL: expected URL: %q, actual: %q", name, tc.expected, actual)
+			}
+		})
+	}
+}
