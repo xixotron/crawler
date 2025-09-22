6dbd8ea4da4870c93be06f5e4ea14195e41340de
xixotron <xixotron89@gmail.com>
2025-09-22 11:51:01 +0200

    get base url from cli args

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	baseUrl := args[1]
+	fmt.Println("starting crawl of:", baseUrl)
 }
