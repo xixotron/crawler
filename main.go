0ed940036c9bde7b6c69e7f7e75f60681c2896a4
xixotron <xixotron89@gmail.com>
2025-09-22 11:54:09 +0200

    remove intermediate args variable

--- main.go
+++ main.go
@@ -6,15 +6,14 @@ import (
 )
 
 func main() {
-	args := os.Args
-	if len(args) < 2 {
+	if len(os.Args) < 2 {
 		fmt.Println("no website provided")
 		os.Exit(1)
 	}
-	if len(args) > 2 {
+	if len(os.Args) > 2 {
 		fmt.Println("too many arguments provided")
 		os.Exit(1)
 	}
-	baseUrl := args[1]
-	fmt.Println("starting crawl of:", baseUrl)
+	baseRawURL := os.Args[1]
+	fmt.Printf("starting crawl of: %s...\n", baseRawURL)
 }
